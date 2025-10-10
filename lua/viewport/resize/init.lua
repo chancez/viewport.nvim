c81c4343e11ad88aaf1bea8495e1104de2e290ad
Chance Zibolski <chance.zibolski@gmail.com>
2025-10-10 15:02:07 -0700

    resize: Add preset mappings
    Signed-off-by: Chance Zibolski <chance.zibolski@gmail.com>

--- lua/viewport/resize/init.lua
+++ lua/viewport/resize/init.lua
@@ -5,14 +5,27 @@ local M = {}
 
 M.actions = actions
 
-local default_config = {
-  resize_amount = 1,
-  mappings = {
+local preset_mappings = {
+  absolute = {
     ['k'] = actions.resize_up,
     ['j'] = actions.resize_down,
     ['h'] = actions.resize_left,
     ['l'] = actions.resize_right,
     ['<Esc>'] = 'stop',
+  },
+  relative = {
+    ['k'] = actions.relative_resize_up,
+    ['j'] = actions.relative_resize_down,
+    ['h'] = actions.relative_resize_left,
+    ['l'] = actions.relative_resize_right,
+    ['<Esc>'] = 'stop',
+  },
+}
+
+local default_config = {
+  resize_amount = 1,
+  mappings = {
+    preset = 'absolute', -- 'absolute' or 'relative'
   },
 }
 
@@ -21,8 +34,21 @@ local resize_mode = nil
 
 M.setup = function(input_cfg)
   config = vim.tbl_deep_extend('force', default_config, input_cfg or {})
+  --
+  -- Deep copy because we're going to mutate the values potentially
+  local mappings = vim.deepcopy(config.mappings)
+  if mappings.preset then
+    local preset = preset_mappings[config.mappings.preset]
+    if not preset then
+      error("Invalid preset: " .. tostring(config.mappings.preset))
+    end
+    -- Delete the preset key so it isnt treated as a mapping itself
+    mappings.preset = nil
+    mappings = vim.tbl_extend('force', preset, mappings)
+  end
+
   resize_mode = mode.new({
-    mappings = config.mappings,
+    mappings = mappings,
   })
 end
 
