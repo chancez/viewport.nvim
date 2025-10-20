d699e43a8531c8c0ad114dce055ee867c30f097e
Chance Zibolski <chance.zibolski@gmail.com>
2025-10-19 17:13:27 -0700

    Document utils.debounce
    Signed-off-by: Chance Zibolski <chance.zibolski@gmail.com>

--- lua/viewport/utils.lua
+++ lua/viewport/utils.lua
@@ -1,5 +1,10 @@
 local utils = {}
 
+-- Debounces a function, ensuring it is only called after a specified delay
+-- since the last invocation.
+-- @param func The function to debounce.
+-- @param delay_ms The delay in milliseconds.
+-- @return A debounced version of the function.
 function utils.debounce(func, delay_ms)
   local timer = vim.uv.new_timer()
   local last_call_args = {}
